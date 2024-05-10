b5ba54695224a935a4dd516b0bcb4fef9836cc23
maugois <gois.mauricio12@gmail.com>
2024-05-09 22:17:16 -0300

    API github: buscar usuarios - Nome e Imagem

--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useRef, useState } from "react";
 
 const ProfileComponente = () => {
 
+  const [input, setInput] = useState('');
+  const [profiles, setProfiles] = useState([]);
+  const [search , setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const countSearch = useRef(0);
+
+  useEffect(() => {
+    if(input.length < 3) {
+      return 
+    }
+
+    fetch(`https://api.github.com/users/${input}`)
+    .then((response) => {
+      if(!response.ok) {
+        throw Error();
+      }
+      return response.json()
+    })
+    .then((data) => {
+      setProfiles([data])
+      countSearch.current += 1
+      setInput('')
+    })
+    .catch((error) => {
+      alert(`Ocorreu um erro ao buscar o perfil: ${input} \n Error: ${error}`);
+    })
+  }, [search]);
+
   const handleSearch = () => {
+    setSearch()
   };
 
   return (
@@ -16,18 +47,35 @@ const ProfileComponente = () => {
         <input
           type="text"
           placeholder="Digite o nome do perfil"
+          onChange={(event) => setInput(event.currentTarget.value)}
+          value={input}
+          disabled={loading}
         />
 
-        <button onClick={handleSearch}>
+        <button onClick={handleSearch} disabled={loading}>
           Buscar
         </button>
 
-        {<p>Carregando...</p>}
+        {loading && <p>Carregando...</p>}
       </div>
+
+      {profiles.length ? (
+        profiles.map((item) => {
+          return (
+            <div key={item.id} className="user-card">
+              <img src={item.avatar_url} alt={item.login} />
+
+              <h2>{item.login}</h2>
+            </div>
+          )
+        })
+      ) : (
+        <p className="no-results">Nenhum perfil encontrado</p>
+      )}
 
       <div className="counter-section">
         <p>
-          Quantidade de perfis pesquisados: <strong></strong>
+          Quantidade de perfis pesquisados: <strong>{countSearch.current}</strong>
         </p>
       </div>
     </main>
